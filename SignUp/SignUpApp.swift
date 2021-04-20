fb43570761f3341b0c2f8f9af07760be37f392c8
Desiree Jallow <60536540+desiree-jallow@users.noreply.github.com>
2021-04-20 16:07:10 -0500

    Initial Commit

---
+++ SignUp/SignUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpApp.swift
+//  SignUp
+//
+//  Created by Desiree on 4/20/21.
+//
+
+import SwiftUI
+
+@main
+struct SignUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
