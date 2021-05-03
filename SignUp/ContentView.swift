cf896185f8429c7764513b8b597b96eb86b6c10c
Desiree Jallow <60536540+desiree-jallow@users.noreply.github.com>
2021-05-03 15:58:48 -0500

    create avatar view

--- SignUp/ContentView.swift
+++ SignUp/ContentView.swift
@@ -9,8 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(#colorLiteral(red: 0.101645493, green: 0.2374779485, blue: 0.3960759271, alpha: 1))
+                .ignoresSafeArea()
+            AvatarView()
+        }
+        
+       
+    }
+   
+}
+
+struct AvatarView: View {
+    var body: some View {
+        HStack {
+            VStack {
+                Image("child")
+                    .resizable()
+                Text("CHILD")
+                    .font(.caption2)
+                    .fontWeight(.bold)
+                    .foregroundColor(.orange)
+            }
+            VStack {
+                Image("parent")
+                    .resizable()
+                Text("PARENT")
+                    .font(.caption2)
+                    .fontWeight(.bold)
+                    .foregroundColor(.green)
+            }
+           
+            VStack {
+                Image("teacher")
+                    .resizable()
+                Text("TEACHER")
+                    .font(.caption2)
+                    .fontWeight(.bold)
+                    .foregroundColor(.red)
+            }
+           
+           
+        }
+        .frame(width: 300, height: 110, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
     }
 }
 
