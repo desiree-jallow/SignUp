3a3e222661aa7b62848e14ce1c99abaa3743385c
Desiree Jallow <60536540+desiree-jallow@users.noreply.github.com>
2021-05-10 18:43:53 -0500

    password confirmation complete

--- SignUp/ContentView.swift
+++ SignUp/ContentView.swift
@@ -12,15 +12,15 @@ struct ContentView: View {
     @State var childButton =  AvatarView(imageName: "child", name: "child", color: Color(#colorLiteral(red: 0.6707887158, green: 0.1774512484, blue: 0.237904652, alpha: 1)))
     @State var teacherButton = AvatarView(imageName: "teacher", name: "teacher", color: Color(#colorLiteral(red: 0, green: 0.5, blue: 0.5, alpha: 1)))
     @StateObject var backgroundSettings = BackgroundSettings()
-//    @State var backgroundColor = Color(#colorLiteral(red: 0.101645493, green: 0.2374779485, blue: 0.3960759271, alpha: 0.8567087602))
     @State var signupDisabled = true
+ 
     var body: some View {
         
         ZStack {
             backgroundSettings.backgroundColor
                 .ignoresSafeArea()
             
-            VStack {
+            VStack(spacing: 20) {
                 Text("Sign Up")
                     .foregroundColor(.white)
                     .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
@@ -56,10 +56,14 @@ struct ContentView: View {
                             parentButton.isSelected = false
                             childButton.isSelected = false
                 }
+                    
             }
+              
                 
                 
                 TextFieldView(signupDisabled: $signupDisabled)
+             
+         
                 
                 Button(action: {
                     print("Hello")
@@ -76,6 +80,7 @@ struct ContentView: View {
                        
                     
                 }).disabled(signupDisabled)
+                
                 HStack {
                     Text("Already have an account.")
                         .bold()
@@ -97,139 +102,141 @@ struct TextFieldView: View {
     @State var email: String = ""
     @State var password: String = ""
     @State var confirmPassword: String = ""
+    @State var showRedBorder = false
     @Binding var signupDisabled: Bool
+    @State var passwordConfirmed = false
+    @EnvironmentObject var backgroundSettings: BackgroundSettings
    
    
     
     var body: some View {
-        VStack(spacing: -10) {
-            ZStack(alignment:.leading) {
-                   
-                TextField("", text: $username)
-                    .padding()
-                    .textFieldStyle(CustomTextFieldStyle())
-             
-                   
+        VStack(spacing: 20) {
 
-                    if username.isEmpty {
-                        HStack {
-                        Image("user")
-                            .resizable()
-                            .frame(width: 20, height: 20, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                            .padding(.leading, 30.0)
-                        
-                        Text("Username")
-                            .foregroundColor(Color(#colorLiteral(red: 0.3242850006, green: 0.36008057, blue: 0.4058655798, alpha: 1)))
-                        
-                    }
-                
-                    }
-            }
             
             ZStack(alignment:.leading) {
-                   
-                TextField("", text: $email, onEditingChanged: { _ in
-                    print("hello")
-                }, onCommit: {
-                    print("hey girl")
-                    if email.isValidEmail {
-                        signupDisabled = false
-                    }
-                })
-                    .padding()
-                    .textFieldStyle(CustomTextFieldStyle())
-             
-               
-
-                    if email.isEmpty {
-                        HStack {
-                            Image("envelope")
-                                .resizable()
-                                .frame(width: 20, height: 20, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                                .padding(.leading, 30.0)
-                            Text("Email")
-                                .foregroundColor(Color(#colorLiteral(red: 0.3242850006, green: 0.36008057, blue: 0.4058655798, alpha: 1)))
+                HStack {
+                    TextFieldImageView(imageName: "user")
+                    
+                    TextField("Username", text: $username, onEditingChanged: { _ in
+                        print("hello")
+                    }, onCommit: {
+                        print("hey girl")
                         
-                    }
+                    })
+                        .padding()
+ 
                 }
+                .modifier(TextFieldModifier())
                
+            }
+            
+            
+            ZStack(alignment:.leading) {
+                HStack {
+                    TextFieldImageView(imageName: "envelope")
+                    
+                    TextField("Email", text: $email, onEditingChanged: { _ in
+                        print("hello")
+                    }, onCommit: {
+                        print("hey girl")
+                        if email.isValidEmail {
+                            signupDisabled = false
+                        }
+                    })
+                        .padding()
+ 
+                }
+
+                .modifier(TextFieldModifier())
             }
             
             ZStack(alignment:.leading) {
-                   
-                TextField("", text: $password, onEditingChanged: { _ in
-                    print("password typing")
-                }, onCommit: {
-                    print("password complete")
-                })
-                    .padding()
-                    .textFieldStyle(CustomTextFieldStyle())
-             
-                   
-                
-              
-
-                    if password.isEmpty {
-                        HStack {
-                            Image("lock")
-                                .resizable()
-                                .frame(width: 20, height: 20, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                                .padding(.leading, 30.0)
+                HStack {
+                    TextFieldImageView(imageName: "lock")
+                    
+                    TextField("Password", text: $password, onEditingChanged: { _ in
+                        print("password typing")
+                    }, onCommit: {
+                        if password.count == 6 {
+                            passwordConfirmed = true
+                        } else {
+                            passwordConfirmed = false
+                        }
+                        print("password complete")
+                    })
                         
-                            Text("Password")
-                                .foregroundColor(Color(#colorLiteral(red: 0.3242850006, green: 0.36008057, blue: 0.4058655798, alpha: 1)))
-                        
-                    }
+                        .padding()
+  
                 }
-               
+                .modifier(TextFieldModifier())
             }
-            
+      
             ZStack(alignment:.leading) {
-                   
-                TextField("", text: $confirmPassword, onEditingChanged: { _ in
-                    print("typing confirm")
-                }, onCommit: {
-                    print("confirm complete")
-                })
-                    .padding()
-                    .textFieldStyle(CustomTextFieldStyle())
-             
-                   
-                
-               
-
-                    if confirmPassword.isEmpty {
-                        HStack {
-                            Image("lock")
-                                .resizable()
-                                .frame(width: 20, height: 20, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                                .padding(.leading, 30.0)
-                            Text("Confirm Password")
-                                .foregroundColor(Color(#colorLiteral(red: 0.3242850006, green: 0.36008057, blue: 0.4058655798, alpha: 1)))
+                HStack {
+                    TextFieldImageView(imageName: "lock")
+                    
+                    TextField("Confirm Password", text: $confirmPassword, onCommit: {
                         
-                    }
+                            print("confirm complete")
+                        
+                      
+                    })
+                    
+                    .onChange(of: confirmPassword, perform: { value in
+                        let range = confirmPassword.startIndex..<confirmPassword.endIndex
+                        if confirmPassword.count <= password.count {
+                            if value != password[range] {
+                                showRedBorder = true
+                            } else {
+                                showRedBorder = false
+                            }
+                        } else {
+                            showRedBorder = true
+                        }
+                        
+                    })
+                        .disabled(passwordConfirmed == false)
+                        .padding()
+                       
+    
                 }
-               
-            }
-            
-        }
-
-    }
-}
-struct CustomTextFieldStyle: TextFieldStyle {
-    @EnvironmentObject var backgroundSettings: BackgroundSettings
-    func _body(configuration: TextField<Self._Label>) -> some View {
-            configuration
-                
-                .padding(15)
                 .background(backgroundSettings.backgroundColor)
                 .cornerRadius(30)
                 .shadow(color: .gray, radius: 5)
-                .overlay(RoundedRectangle(cornerRadius: 30).stroke(Color(#colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)), lineWidth: 1))
+                .overlay(showRedBorder ? RoundedRectangle(cornerRadius: 30).stroke(Color(.red), lineWidth: 1) : RoundedRectangle(cornerRadius: 30).stroke(Color(#colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)), lineWidth: 1))
+//                .modifier(TextFieldModifier())
+            }
+           
         }
+        .frame(width: 350)
+        
+        
+    }
+}
+
+
+struct TextFieldModifier: ViewModifier {
+    @EnvironmentObject var backgroundSettings: BackgroundSettings
+        func body(content: Content) -> some View {
+        content
+            .background(backgroundSettings.backgroundColor)
+            .cornerRadius(30)
+            .shadow(color: .gray, radius: 5)
+            .overlay(RoundedRectangle(cornerRadius: 30).stroke(Color(#colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)), lineWidth: 1))
+    }
     
     
 }
+struct TextFieldImageView: View {
+    var imageName: String
+    var body: some View {
+        Image(imageName)
+            .resizable()
+            .frame(width: 20, height: 20, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            .padding(.leading, 30.0)
+    }
+}
+                    
 
 struct AvatarView: View {
     var imageName: String
